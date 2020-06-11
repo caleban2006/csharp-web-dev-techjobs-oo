4bb9496881191f3f7e0d3a5d079165ac440b8ac0
caleban2006 <logan.jen.2005@gmail.com>
2020-06-11 15:54:10 -0400

    completed job class

--- TechJobsOO/Location.cs
+++ TechJobsOO/Location.cs
@@ -15,7 +15,7 @@ namespace TechJobsOO
         
         // TODO: Add a second constructor to this class that uses the Location() constructor and sets the value of the value field.
 
-         public Location(string locationValue) : this()
+        public Location(string locationValue) : this()
         {
             Value = locationValue;
         }
